4a21c254b0291fb29b0880ccf9523f7dcc5dac31
fpy <13591272948@163.com>
2024-02-02 15:11:29 +0800

    添加项目文件。

---
+++ TraverseFiles/FormTraverseFiles.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace TraverseFiles {
+    partial class FormTraverseFiles {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.BtnChooseDir = new System.Windows.Forms.Button();
+            this.TxtDirPath = new System.Windows.Forms.TextBox();
+            this.TxtFiles = new System.Windows.Forms.TextBox();
+            this.BtnCopy = new System.Windows.Forms.Button();
+            this.ChkContainSubFolder = new System.Windows.Forms.CheckBox();
+            this.ChkNotFullPath = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // BtnChooseDir
+            // 
+            this.BtnChooseDir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BtnChooseDir.Font = new System.Drawing.Font("宋体", 10.8F);
+            this.BtnChooseDir.Location = new System.Drawing.Point(629, 58);
+            this.BtnChooseDir.Name = "BtnChooseDir";
+            this.BtnChooseDir.Size = new System.Drawing.Size(120, 58);
+            this.BtnChooseDir.TabIndex = 0;
+            this.BtnChooseDir.Text = "选择文件夹";
+            this.BtnChooseDir.UseVisualStyleBackColor = false;
+            this.BtnChooseDir.Click += new System.EventHandler(this.BtnChooseDir_Click);
+            // 
+            // TxtDirPath
+            // 
+            this.TxtDirPath.Location = new System.Drawing.Point(59, 77);
+            this.TxtDirPath.Name = "TxtDirPath";
+            this.TxtDirPath.Size = new System.Drawing.Size(480, 25);
+            this.TxtDirPath.TabIndex = 1;
+            // 
+            // TxtFiles
+            // 
+            this.TxtFiles.Location = new System.Drawing.Point(59, 150);
+            this.TxtFiles.Multiline = true;
+            this.TxtFiles.Name = "TxtFiles";
+            this.TxtFiles.Size = new System.Drawing.Size(670, 216);
+            this.TxtFiles.TabIndex = 2;
+            // 
+            // BtnCopy
+            // 
+            this.BtnCopy.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BtnCopy.Font = new System.Drawing.Font("宋体", 10.8F);
+            this.BtnCopy.Location = new System.Drawing.Point(59, 378);
+            this.BtnCopy.Name = "BtnCopy";
+            this.BtnCopy.Size = new System.Drawing.Size(182, 58);
+            this.BtnCopy.TabIndex = 3;
+            this.BtnCopy.Text = "复制到剪切板";
+            this.BtnCopy.UseVisualStyleBackColor = false;
+            this.BtnCopy.Click += new System.EventHandler(this.BtnCopy_Click);
+            // 
+            // ChkContainSubFolder
+            // 
+            this.ChkContainSubFolder.AutoSize = true;
+            this.ChkContainSubFolder.Font = new System.Drawing.Font("宋体", 10.8F);
+            this.ChkContainSubFolder.Location = new System.Drawing.Point(59, 108);
+            this.ChkContainSubFolder.Name = "ChkContainSubFolder";
+            this.ChkContainSubFolder.Size = new System.Drawing.Size(145, 23);
+            this.ChkContainSubFolder.TabIndex = 4;
+            this.ChkContainSubFolder.Text = "包含子文件夹";
+            this.ChkContainSubFolder.UseVisualStyleBackColor = true;
+            // 
+            // ChkNotFullPath
+            // 
+            this.ChkNotFullPath.AutoSize = true;
+            this.ChkNotFullPath.Font = new System.Drawing.Font("宋体", 10.8F);
+            this.ChkNotFullPath.Location = new System.Drawing.Point(336, 108);
+            this.ChkNotFullPath.Name = "ChkNotFullPath";
+            this.ChkNotFullPath.Size = new System.Drawing.Size(145, 23);
+            this.ChkNotFullPath.TabIndex = 5;
+            this.ChkNotFullPath.Text = "不输出全路径";
+            this.ChkNotFullPath.UseVisualStyleBackColor = true;
+            // 
+            // FormTraverseFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ChkNotFullPath);
+            this.Controls.Add(this.ChkContainSubFolder);
+            this.Controls.Add(this.BtnCopy);
+            this.Controls.Add(this.TxtFiles);
+            this.Controls.Add(this.TxtDirPath);
+            this.Controls.Add(this.BtnChooseDir);
+            this.Name = "FormTraverseFiles";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormTraverseFiles_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnChooseDir;
+        private System.Windows.Forms.TextBox TxtDirPath;
+        private System.Windows.Forms.TextBox TxtFiles;
+        private System.Windows.Forms.Button BtnCopy;
+        private System.Windows.Forms.CheckBox ChkContainSubFolder;
+        private System.Windows.Forms.CheckBox ChkNotFullPath;
+    }
+}
+
---
+++ TraverseFiles/FormTraverseFiles.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Resources;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using TraverseFiles.Properties;
+
+namespace TraverseFiles {
+    public partial class FormTraverseFiles : Form {
+
+        private static Color ColorFromHexByArgb(string colorCode) {
+            if(colorCode [0] != '#')
+                throw new ArgumentException("Invalid color code format");
+
+            int alpha = int.Parse(colorCode.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+            int red = int.Parse(colorCode.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+            int green = int.Parse(colorCode.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+            int blue = int.Parse(colorCode.Substring(7, 2), System.Globalization.NumberStyles.HexNumber);
+
+            return Color.FromArgb(alpha, red, green, blue);
+        }
+
+        private static Color ColorFromHexByRgb(string colorCode) {
+            if(colorCode [0] != '#')
+                throw new ArgumentException("Invalid color code format");
+
+            int red = int.Parse(colorCode.Substring(1, 2), System.Globalization.NumberStyles.HexNumber);
+            int green = int.Parse(colorCode.Substring(3, 2), System.Globalization.NumberStyles.HexNumber);
+            int blue = int.Parse(colorCode.Substring(5, 2), System.Globalization.NumberStyles.HexNumber);
+
+            return Color.FromArgb(red,green,blue);
+        }
+
+        private const int rgbColorHexCodeLength = 7;
+        private const int argbColorHexCodeLength = 9;
+
+        private const int subFilePadLeftNumber = 2;
+
+        private static Color ColorFromHexCode(string colorCode) {
+            if(!colorCode.StartsWith("#"))
+                colorCode = colorCode.Insert(0, "#");
+            
+            if(colorCode.Length != rgbColorHexCodeLength && colorCode.Length != argbColorHexCodeLength)
+                throw new ArgumentException("Invalid color code format");
+
+            if(colorCode.Length == rgbColorHexCodeLength)
+                return ColorFromHexByRgb(colorCode);
+            else
+                return ColorFromHexByArgb(colorCode);
+        }
+
+        private static string TraverseFolder(string folderPath, bool containSubFolder = false, bool notFullPath = false, int addBlankNumber = 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            // 获取文件夹中的所有文件名
+            // Get all file names in the folder
+            string [] fileNames = Directory.GetFiles(folderPath);
+
+            stringBuilder.Append(string.Empty.PadLeft(addBlankNumber * subFilePadLeftNumber));
+            // 遍历文件名
+            // Traverse file names
+            foreach(string fileName in fileNames) {
+                if(notFullPath) {
+                    string fixedfileName = fileName.Remove(0, folderPath.Length + 1);
+                    stringBuilder.Append(fixedfileName);
+                } else {
+                    stringBuilder.Append(fileName);
+                }
+                stringBuilder.Append(Environment.NewLine);
+            }
+
+
+            if(containSubFolder) {
+                // 获取文件夹中的所有子文件夹
+                // Get all sub folders in the folder
+                string [] subDirectories = Directory.GetDirectories(folderPath);
+
+                // 递归遍历每个子文件夹
+                // Recursively traverse each sub folder
+                foreach(string subDirectory in subDirectories) {
+                    stringBuilder.Append(Environment.NewLine).Append(string.Empty.PadLeft(addBlankNumber * subFilePadLeftNumber));
+                    if(notFullPath) {
+                        string fixedfileName = subDirectory.Remove(0, folderPath.Length + 1);
+                        stringBuilder.Append(fixedfileName);
+                    } else {
+                        stringBuilder.Append(subDirectory);
+                    }
+                    stringBuilder.Append(':').Append(Environment.NewLine);
+                    string childFileNames = TraverseFolder(subDirectory, containSubFolder, notFullPath, addBlankNumber + 1);
+                    stringBuilder.Append(childFileNames).Append(Environment.NewLine);                   
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+        public void ClearCtrl(Control parContainer) {
+            for(int index = 0; index < parContainer.Controls.Count; index++) {
+                // 如果是容器类控件，递归调用
+                // If it is a container control, recursively call
+                if(parContainer.Controls [index].HasChildren) {
+                    ClearCtrl(parContainer.Controls [index]);
+                } else {
+                    switch(parContainer.Controls [index].GetType().Name) {
+                        case "TextBox":
+                            parContainer.Controls [index].Text = "";
+                            break;
+                        case "RadioButton":
+                            break;
+                        case "CheckBox":
+                            break;
+                        case "ComboBox":
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void SetCtrl(Control fatherControl) {
+            foreach(Control childControl in fatherControl.Controls) {
+                string text = GetResourceString(childControl.Name + "Text");
+                if(!string.IsNullOrEmpty(text)) {
+                    childControl.Text = text;
+                }
+                if(childControl.HasChildren) {
+                    SetCtrl(childControl);
+                } else {
+                    switch(childControl.GetType().Name) {
+                        case "Button":
+                            ((Button)childControl).BackColor = ColorFromHexCode("8E8F90");
+                            break;
+                    }
+                }
+            }
+        }
+
+        static ResourceManager resourceManager;
+
+        private string GetResourceString(string name) {
+            return resourceManager.GetString(this.Name + "." + name, Thread.CurrentThread.CurrentUICulture);
+        }
+
+        static FormTraverseFiles() {
+            CultureInfo cultureInfo = Thread.CurrentThread.CurrentUICulture;
+            if(cultureInfo.Name.Contains("CN")) {
+                resourceManager = new ResourceManager("TraverseFiles.Properties.Resource_zh-CN", typeof(Resource_zh_CN).Assembly);
+            } else {
+                resourceManager = new ResourceManager("TraverseFiles.Properties.Resource_en-US", typeof(Resource_en_US).Assembly);
+            }
+        }
+
+        public FormTraverseFiles() {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.WindowState = FormWindowState.Normal;
+            this.Text = GetResourceString("FormText");
+            this.BackColor = ColorFromHexCode("F7F7F7");            
+        }
+
+        private void FormTraverseFiles_Load(object sender, EventArgs e) {
+            SetCtrl(this);
+            this.TxtFiles.ScrollBars = ScrollBars.Both;
+        }
+
+        private void BtnChooseDir_Click(object sender, EventArgs e) {
+            ClearCtrl(this);
+            using(FolderBrowserDialog folderDialog = new FolderBrowserDialog()) {
+                DialogResult dialogResult = folderDialog.ShowDialog(this);
+                if(dialogResult == DialogResult.OK) {
+                    TxtDirPath.Text = folderDialog.SelectedPath;
+                    // 遍历文件夹及其所有子文件夹中的文件名
+                    // Traverse the file names in a folder and all its subfolders
+                    string subFileNames = TraverseFolder(folderDialog.SelectedPath, ChkContainSubFolder.Checked, ChkNotFullPath.Checked);
+                    TxtFiles.Text = subFileNames;
+                }
+            }
+        }
+
+        private void BtnCopy_Click(object sender, EventArgs e) {
+            if(!string.IsNullOrEmpty(TxtFiles.Text))
+                Clipboard.SetText(TxtFiles.Text);
+            MessageBox.Show(GetResourceString("BtnCopyText"));
+        }
+    }
+}
